2e965c3a1fbe2148f74cb6ceb088684d5c4558a8
MayurSChittaragi <mayurschittaragi@gmail.com>
2023-02-01 10:35:41 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,99 +24,99 @@ import Contact from "./pages/contact/Contact";
 axios.defaults.withCredentials = true;
 
 function App() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    async function loginStatus() {
-      const status = await getLoginStatus();
-      dispatch(SET_LOGIN(status));
-    }
-    loginStatus();
-  }, [dispatch]);
+	useEffect(() => {
+		async function loginStatus() {
+			const status = await getLoginStatus();
+			dispatch(SET_LOGIN(status));
+		}
+		loginStatus();
+	}, [dispatch]);
 
-  return (
-    <BrowserRouter>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot" element={<Forgot />} />
-        <Route path="/resetpassword/:resetToken" element={<Reset />} />
+	return (
+		<BrowserRouter>
+			<ToastContainer />
+			<Routes>
+				{/* <Route path="/" element={<Home />} /> */}
+				<Route path="/" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/forgot" element={<Forgot />} />
+				<Route path="/resetpassword/:resetToken" element={<Reset />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <Sidebar>
-              <Layout>
-                <Dashboard />
-              </Layout>
-            </Sidebar>
-          }
-        />
-        <Route
-          path="/add-product"
-          element={
-            <Sidebar>
-              <Layout>
-                <AddProduct />
-              </Layout>
-            </Sidebar>
-          }
-        />
-        <Route
-          path="/product-detail/:id"
-          element={
-            <Sidebar>
-              <Layout>
-                <ProductDetail />
-              </Layout>
-            </Sidebar>
-          }
-        />
-        <Route
-          path="/edit-product/:id"
-          element={
-            <Sidebar>
-              <Layout>
-                <EditProduct />
-              </Layout>
-            </Sidebar>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <Sidebar>
-              <Layout>
-                <Profile />
-              </Layout>
-            </Sidebar>
-          }
-        />
-        <Route
-          path="/edit-profile"
-          element={
-            <Sidebar>
-              <Layout>
-                <EditProfile />
-              </Layout>
-            </Sidebar>
-          }
-        />
-        <Route
-          path="/contact-us"
-          element={
-            <Sidebar>
-              <Layout>
-                <Contact />
-              </Layout>
-            </Sidebar>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+				<Route
+					path="/dashboard"
+					element={
+						<Sidebar>
+							<Layout>
+								<Dashboard />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+				<Route
+					path="/add-product"
+					element={
+						<Sidebar>
+							<Layout>
+								<AddProduct />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+				<Route
+					path="/product-detail/:id"
+					element={
+						<Sidebar>
+							<Layout>
+								<ProductDetail />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+				<Route
+					path="/edit-product/:id"
+					element={
+						<Sidebar>
+							<Layout>
+								<EditProduct />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+				<Route
+					path="/profile"
+					element={
+						<Sidebar>
+							<Layout>
+								<Profile />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+				<Route
+					path="/edit-profile"
+					element={
+						<Sidebar>
+							<Layout>
+								<EditProfile />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+				<Route
+					path="/contact-us"
+					element={
+						<Sidebar>
+							<Layout>
+								<Contact />
+							</Layout>
+						</Sidebar>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
